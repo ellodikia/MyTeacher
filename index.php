552d849886d10933dc1f02248dcb930d7c93138e
ellodikia <gabriellaodikia@gmail.com>
2026-01-06 21:12:53 +0700

    update 6

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="myteacher, MYTEACHER, my teacher, MY TEACHER">
     <title>MYTEACHER</title>
     <link rel="icon" type="image/jpeg" href="img/logo.jpeg">
     <script src="https://cdn.tailwindcss.com"></script>
